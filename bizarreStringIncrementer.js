bdb900e36b9b1593c34f97c7682f61361951414e
rio14 <riteshsinha14.rs@gmail.com>
2020-12-15 15:49:12 +0530

    added bizarreStringIncrementer function

--- bizarreStringIncrementer.js
+++ bizarreStringIncrementer.js
@@ -1,1 +1,23 @@
 // Start your implementation here
+const bizarreStringIncrementer = (arg) => {
+  let regexp = /\d+$/g;
+  if (arg.match(regexp)) {
+    let number = parseInt(arg.match(regexp)[0]);
+    number += 1;
+    arg = arg.replace(/\d+$/g, "");
+    arg += number;
+  } else {
+    arg += "1";
+  }
+  return arg;
+};
+
+console.log(bizarreStringIncrementer("foo"));
+console.log(bizarreStringIncrementer("foo23"));
+console.log(bizarreStringIncrementer("foo0041"));
+console.log(bizarreStringIncrementer("foo9"));
+console.log(bizarreStringIncrementer("foo099"));
+console.log(bizarreStringIncrementer("f99oo"));
+console.log(bizarreStringIncrementer("f99oo23"));
+console.log(bizarreStringIncrementer("f99oo099"));
+console.log(bizarreStringIncrementer("f99oo0099"));
